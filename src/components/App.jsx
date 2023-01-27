bd3c0877c5a5d0d597f0f9516956f7a5562b8eca
olhazamlynska <olya.sonechko@gmail.com>
2023-01-27 11:34:49 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,9 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
       }}
     >
-      React homework template
+      React homework template ddd
     </div>
   );
 };
